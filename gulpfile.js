67f3d7ea8eace2d239a5b3e3987e12d9d756c804
colorlight <colorlightleecaa@gmail.com>
2017-06-10 00:32:32 +0800

    完成了wiredep路径如何修改，请使用**

--- gulpfile.js
+++ gulpfile.js
@@ -164,7 +164,7 @@ gulp.task('wiredep', () => {
   // }))
   // .pipe(gulp.dest('app/styles'));
 
-  gulp.src('app/*.html')
+  gulp.src('app/**/*.html')
     .pipe(wiredep({
       exclude: ['bootstrap-sass'],
       ignorePath: /^(\.\.\/)*\.\./
